8c48da83284e502218cd751069542c1a3c3f086a
aiosf <aotsincere@gmail.com>
2019-10-06 23:32:10 +0700

    update.

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ app.post('/webhook', (req, res) => {
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
+      console.log('\n\n')
+      console.log('just msg: ')
       console.log(webhook_event);
     });
 
