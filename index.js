d2b901da02f41194dff042a329db5bbf0b5c9658
at-syot <aotsincere@gmail.com>
2019-10-07 14:28:06 +0700

    prepare static resource.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json())
 
+app.use(express.static('public'))
+
 const request = require('request')
 
 // read .env file.
@@ -15,8 +17,28 @@ function handleMessage(sender_psid, received_message) {
   console.log(received_message)
   let res
   if (received_message.text) {
-    res = {
-      text: `You send this msg: ${received_message.text} back to the page!.`
+    const text = received_message.text
+    if (text == 'start') {
+      res = {
+        "text": "Pick a color:",
+        "quick_replies":[
+          {
+            "content_type":"text",
+            "title":"Red",
+            "payload": "",
+            "image_url":"https://www.iconfinder.com/icons/3668841/earnings_income_salary_wallet_icon"
+          },{
+            "content_type":"text",
+            "title":"Green",
+            "payload":"<POSTBACK_PAYLOAD>",
+            "image_url":"http://example.com/img/green.png"
+          }
+        ]
+      }
+    } else {
+      res = {
+        text: `You send this msg: ${text} back to the page!.`
+      }
     }
   }
 
@@ -61,10 +83,9 @@ function handlePostback(sender_psid, received_postback) {
       return {
         text: 'OK, this is the right photo.'
       }
-    } else {
-      return { 
-        text: 'Ops! Try upload another one.'
-      }
+    }
+    return { 
+      text: 'Ops! Try upload another one.'
     }
   })())
 }
@@ -76,7 +97,6 @@ function callSendAPI(sender_psid, res) {
     },
     message: res
   }
-  console.log('request_body: ', request_body)
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
     qs: { "access_token": PAGE_ACCESS_TOKEN },
@@ -99,12 +119,8 @@ app.post('/webhook', (req, res) => {
 
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
-
-    // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       let sender_psid = webhook_event.sender.id
       console.log('webhook event.')
@@ -116,7 +132,6 @@ app.post('/webhook', (req, res) => {
       }
     });
 
-    // Returns a '200 OK' response to all requests
     res.status(200).send('EVENT_RECEIVED');
   } else {
     // Returns a '404 Not Found' if event is not from a page subscription
@@ -124,7 +139,6 @@ app.post('/webhook', (req, res) => {
   }
 });
 
-// Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
   console.log('webhook GET got call.')
@@ -155,6 +169,4 @@ app.get('/webhook', (req, res) => {
 
 
 const port = process.env.PORT || 1337
-app.listen(port, function() {
-  console.log(`web hook is listening on port: ${port}.`)
-})
+app.listen(port, () => console.log(`web hook is listening on port: ${port}.`))
